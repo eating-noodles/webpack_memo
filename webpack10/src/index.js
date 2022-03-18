9ff69a7338aeeee8a6daaba609f0b0a53935296a
noodles <1205380933@qq.com>
2022-03-18 20:54:38 +0800

    feat: webpack-tree_shaking

---
+++ webpack10/src/index.js
@@ -0,0 +1,6 @@
+import {
+    add
+} from "./math";
+
+const c = add(1, 2)
+console.log(c)
---
+++ webpack10/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'production',
+    devtool: 'eval-cheap-module-source-map',
+    entry: {
+        main: './src/index.js',
+    },
+    devServer: {
+        static: './dist',
+        open: true,
+        hot: 'only'
+        // hot: false
+    },
+    module: {
+        rules: [{
+                test: /\.(ttf|woff|woff2)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                        }
+                    },
+                    'sass-loader',
+                    'postcss-loader'
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'postcss-loader'
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    optimization: {
+        usedExports: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        // new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+        // punlicPath: '/',
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
